aff1aed9d452b57c50a7d1ecfb6ca5748d4b200c
shengyuan wu <pokl50603@gmail.com>
2022-10-03 23:48:31 +0800

    Initial Commit

---
+++ a209HelloTableViewUITests/a209HelloTableViewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  a209HelloTableViewUITestsLaunchTests.swift
+//  a209HelloTableViewUITests
+//
+//  Created by shengyuan on 2022/10/3.
+//
+
+import XCTest
+
+final class a209HelloTableViewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
